baab49f26bfc1cf8b23d3dea6bf8c39f493505e5
mohamed-ahmed <mohamed.ahmed50050073@gmail.com>
2026-06-23 17:18:08 +0300

    ..

--- lib/controller/app_config_provider.dart
+++ lib/controller/app_config_provider.dart
@@ -1,7 +1,7 @@
 class AppConfigProvider {
   // ================ApiUrl==================//
-  static const String apiUrl = "https://aventra-co.com/staging/server/userAPI/";
+  static const String apiUrl = "https://aventra-co.com/app/server/userAPI/";
 
   //=================imageURL===============//
-  static const String imageURL = "https://aventra-co.com/staging/server/uploads/";
+  static const String imageURL = "https://aventra-co.com/app/server/uploads/";
 }
